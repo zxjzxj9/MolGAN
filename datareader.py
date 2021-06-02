61cd38eaac239990e50c3956fd2d15445fa2dc9f
Victor Zhang <zxjzxj9@gmail.com>
2021-06-02 22:35:59 +0800

    Add some basic dgl code

--- datareader.py
+++ datareader.py
@@ -2,6 +2,7 @@
 
 import tarfile
 from torch.utils.data import Dataset
+import numpy as np
 import rdkit
 
 class QM9BZ2Dataset(Dataset):
---
+++ model.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+import dgl
+from dgl.nn.pytorch import GraphConv
+import torch.nn as nn
+import torch.nn.functional as F
+
