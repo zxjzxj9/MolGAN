51dee306700f8da5e4c0a51b63898f26ae4e4f17
Victor Zhang <zxjzxj9@gmail.com>
2021-06-03 22:54:41 +0800

    Add some scaffold code

--- model.py
+++ model.py
@@ -1,7 +1,19 @@
 #! /usr/bin/env python
 
 import dgl
+import dgl.function as fn
 from dgl.nn.pytorch import GraphConv
 import torch.nn as nn
 import torch.nn.functional as F
 
+class MolGen(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
