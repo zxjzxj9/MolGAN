a723293cfb88c29bd9ec237f0f6b7e03f31afa5b
Victor Zhang <zxjzxj9@gmail.com>
2021-07-21 21:44:35 +0800

    Add dataloader code

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from model import MolGen, MolDis
 import toml
 import torch
 from datareader import QM9BZ2Dataset
+from torch.utils.data import DataLoader
 
 def train(data, model, opt, niter):
     pass
@@ -27,3 +28,4 @@ if __name__ == "__main__":
     }
 
     ds = QM9BZ2Dataset(conf["data_path"])
+    dl = DataLoader(ds, conf["batch_size"], shuffle=True, num_workers=8, pin_memory=True)
