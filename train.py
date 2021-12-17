e76a18043eec7a9acdc60577fe47a3edfb1d3e95
Victor Zhang <zxjzxj9@gmail.com>
2021-12-17 23:26:42 +0800

    Recover training code

--- train.py
+++ train.py
@@ -96,11 +96,11 @@ if __name__ == "__main__":
     for epoch in range(conf["nepoch"]):
         print("In epoch {:4d}".format(epoch + 1))
         print("Training Stage...")
-        # niter = train(dl, model, optimizer, niter, conf["batch_size"], tau)
+        niter = train(dl, model, optimizer, niter, conf["batch_size"], tau)
         tau *= 0.9
         print("")
         print("Saving model checkpoints...")
-        # torch.save({k: v.state_dict() for k, v in model.items()}, f"model_{epoch:04d}.pt")
+        torch.save({k: v.state_dict() for k, v in model.items()}, f"model_{epoch:04d}.pt")
         print("Done.")
         print("Testing Stage...")
         print(model['gen'])
