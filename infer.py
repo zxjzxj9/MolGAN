4c747df1d6cdfcc12c7123073ca53c4eb4dea9a5
Victor Zhang <zxjzxj9@gmail.com>
2021-12-29 21:33:14 +0800

    Add argument parsing code

--- infer.py
+++ infer.py
@@ -2,7 +2,9 @@
 
 import argparse
 
-
+args = argparse.ArgumentParser("MolGAN inference script")
+args.add_argument("-c", "--config", type=str, default="hparam.toml", help="Config file location")
+opt = args.parse_args()
 
 if __name__ == "__main__":
     pass
