7e39febe52ab6da0cd8dd5724f4a7cae13d20919
Victor Zhang <zxjzxj9@gmail.com>
2021-12-30 21:44:10 +0800

    Add model creation code

--- infer.py
+++ infer.py
@@ -1,10 +1,22 @@
 #! /usr/bin/env python
 
 import argparse
+import toml
+
+from model import MolGen, MolDis
 
 args = argparse.ArgumentParser("MolGAN inference script")
-args.add_argument("-c", "--config", type=str, default="hparam.toml", help="Config file location")
+args.add_argument("-c", "--conf", type=str, default="hparam.toml", help="Config file location")
 opt = args.parse_args()
 
 if __name__ == "__main__":
-    pass
+    with open("hparam.toml", "r") as fin:
+        conf = toml.load(args.conf)
+
+    use_cuda = conf["use_cuda"]
+
+    model = {
+        "gen": MolGen(conf["num_atom"], conf["num_atom_type"], conf["num_bond_type"],
+                      conf["num_hidden"], conf["gen_layer"]),
+        "dis": MolDis(conf["num_atom"], conf["num_atom_type"], conf["num_bond_type"])
+    }
