e6bbfa49f3e07f0997d062b223bc8b8b936e1df8
Victor Zhang <zxjzxj9@gmail.com>
2021-12-31 21:32:13 +0800

    Add model inference code

--- infer.py
+++ infer.py
@@ -2,17 +2,18 @@
 
 import argparse
 import toml
+import torch
 
 from model import MolGen, MolDis
 
 args = argparse.ArgumentParser("MolGAN inference script")
 args.add_argument("-c", "--conf", type=str, default="hparam.toml", help="Config file location")
+args.add_argument("-m", "--model", type=str, default="model.pt", hep="Checkpoint model file")
 opt = args.parse_args()
 
 if __name__ == "__main__":
     with open("hparam.toml", "r") as fin:
         conf = toml.load(args.conf)
-
     use_cuda = conf["use_cuda"]
 
     model = {
@@ -20,3 +21,8 @@ if __name__ == "__main__":
                       conf["num_hidden"], conf["gen_layer"]),
         "dis": MolDis(conf["num_atom"], conf["num_atom_type"], conf["num_bond_type"])
     }
+
+    sd = torch.load(args.model)
+    model["gen"].load_state_dict(sd["gen"])
+    model["dis"].load_state_dict(sd["dis"])
+
