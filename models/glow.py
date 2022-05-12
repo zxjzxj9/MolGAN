e416ac8dbae019eff3ac9462aeda56651c69766d
Victor Zhang <zxjzxj9@gmail.com>
2022-05-12 22:55:22 +0800

    add act norm backbond code

--- models/glow.py
+++ models/glow.py
@@ -1,2 +1,13 @@
 #! /usr/bin/env python
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ActNorm2d(nn.Module):
+
+    def __int__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
